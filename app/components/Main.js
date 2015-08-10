c1829175f9219ed045674be4e8445b7584c50532
David G Chaves <davidgchaves@gmail.com>
2015-08-10 16:45:49 +0200

    Add a route for the new Home Component

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,18 @@
 var React = require('react');
+var Router = require('react-router');
 
 var Main = React.createClass({
   render: function () {
     return (
-      <div>
-        Testing Main Component
+      <div className="main-container">
+        <nav className="navbar navbar-default" role="navigation">
+          <div className="col-sm7 col-sm-offset-2" style={{marginTop: 15}}>
+            Here comes the Menu!!!
+          </div>
+        </nav>
+        <div className="container">
+          <Router.RouteHandler />
+        </div>
       </div>
     );
   }
--- app/config/routes.js
+++ app/config/routes.js
@@ -2,8 +2,10 @@ var React = require('react');
 var Router = require('react-router');
 
 var Main = require('../components/Main');
+var Home = require('../components/Home');
 
 module.exports = (
   <Router.Route name="app" path="/" handler={Main}>
+    <Router.DefaultRoute handler={Home} />
   </Router.Route>
 );
